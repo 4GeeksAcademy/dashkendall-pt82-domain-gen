99dd6cc43752c8c1e60f9017c1d1d42f23916889
dashkendall <131983174+dashkendall@users.noreply.github.com>
2025-06-13 23:07:50 +0000

    feat(domaingenerator): Created domain generator with html/css/js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+// create array for pronouns
+// create function to concat all different domain names
+
+document.getElementById("domain").innerHTML = generateDomain();
+function generateDomain() {
+
+      let pronoun = ['my', 'the', 'their', 'her', 'our', 'your'];
+      let adj = ['clever', 'tiny', 'pretty', 'grumpy', 'adventurous', 'brave', 'happy'];
+      let noun = ['friend', 'road', 'river', 'city', 'flamingo', 'friend', 'corner', 'tree'];
+    
+        let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+        let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+        let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+
+      return `${randomPronoun}${randomAdj}${randomNoun}.com`;
+      
+    }
+
+  let domain = generateDomain();
+  console.log(domain);
+
   console.log("Hello Rigo from the console!");
 };
+ 
